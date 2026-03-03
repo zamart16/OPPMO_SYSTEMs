aef8b1673e9afe102801ac90616f6190bba6969a
zamart16 <154574772+zamart16@users.noreply.github.com>
2026-03-03 08:39:41 +0800

    Update header.blade.php

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -2,7 +2,12 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta http-equiv="Cache-Control" content="no-store, no-cache, must-revalidate">
+  <meta http-equiv="Pragma" content="no-cache">
+  <meta http-equiv="Expires" content="0">
+  <link rel="icon" type="image/x-icon" href="{{ asset('logo.png') }}">
 
+    
   <title>Supplier Evaluation Management</title>
   <script src="https://cdn.tailwindcss.com/3.4.16"></script>
   <link rel="preconnect" href="https://fonts.googleapis.com">
